41d4433236a1b632de15faba2de5794cc202601e
Jordan Finnigan <j@jdo.sh>
2025-02-16 13:15:34 -0500

    Add sorter, update lock

---
+++ lua/plugins/tssorter.lua
@@ -0,0 +1,6 @@
+return {
+  'mtrajano/tssorter.nvim',
+  version = 'main', -- latest stable version, use `main` to keep up with the latest changes
+  ---@module "tssorter"
+  ---@type TssorterOpts
+}
