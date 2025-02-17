8e376785def40a3f53465a583222f611c2b4193b
Jordan Finnigan <j@jdo.sh>
2025-02-17 10:16:20 -0500

    cleanup plugins, update lock

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,7 +19,7 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = '\\'
 
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 vim.opt.number = true
 vim.opt.relativenumber = true
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,15 +1,22 @@
 return { -- Collection of various small independent plugins/modules
   'echasnovski/mini.nvim',
+  version = '*',
   config = function()
     require('mini.ai').setup { n_lines = 500 }
-
+    require('mini.operators').setup()
     require('mini.surround').setup()
-
-    local statusline = require 'mini.statusline'
-    statusline.setup { use_icons = vim.g.have_nerd_font }
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
+    require('mini.comment').setup()
+    require('mini.pairs').setup()
+    require('mini.statusline').setup {
+      use_icons = vim.g.have_nerd_font,
+      section_location = function()
+        return '%2l:%-2v'
+      end,
+    }
+    require('mini.bufremove').setup()
+    require('mini.animate').setup()
+    require('mini.indentscope').setup()
+    require('mini.sessions').setup()
+    require('mini.notify').setup()
   end,
 }
