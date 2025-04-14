e335196625882079f76093963fc13c959d703d62
Jordan Finnigan <j@jdo.sh>
2025-04-14 08:57:04 -0400

    Updates

--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,14 +1,18 @@
 return {
   'mfussenegger/nvim-lint',
   event = { 'BufReadPre', 'BufNewFile' },
-  config = function()
-    local lint = require 'lint'
-    lint.linters_by_ft = {
+  opts = {
+    linters_by_ft = {
       markdown = { 'markdownlint' },
+      go = { 'golangcilint' },
       php = { 'phpstan' },
-    }
-    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-    vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+    },
+  },
+  config = function(_, opts)
+    local lint = require 'lint'
+    lint.linters_by_ft = opts.linters_by_ft
+    local lint_augroup = vim.api.nvim_create_augroup('linting', { clear = true })
+    vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufWritePost', 'InsertLeave' }, {
       group = lint_augroup,
       callback = function()
         if vim.opt_local.modifiable:get() then
